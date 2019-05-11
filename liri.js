817e9f5ed526f3d51ec0ca7ae0821417a8f06fac
Jennifer Duey <jlduey@gmail.com>
2019-05-10 22:34:23 -0400

    stopping for tonight-installed everything i needed and started switch commands

--- liri.js
+++ liri.js
@@ -16,3 +16,34 @@ moment().format();
 
 //npm installs and initialize package.json
 
+let command = process.argv[2];
+let input = process.argv[3];
+
+// 4 different parameters this can take
+//concert-this
+//spotify-this-song
+//movie-this
+//do-what-it-says
+
+switch(command){
+    case 'concert-this':
+
+    console.log("concert command test");
+break;
+
+case 'spotify-this-song':
+
+    console.log("spotify command test");
+break;
+
+case 'movie-this':
+
+    console.log("movie command test");
+break;
+
+case 'do-what-it-says':
+
+    console.log("filesystem command test");
+break;
+
+}
