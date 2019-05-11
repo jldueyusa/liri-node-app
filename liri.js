3b060786e37fc5aa036b42c9d4ade7d6dbdbe348
Jennifer Duey <jlduey@gmail.com>
2019-05-10 22:10:19 -0400

    initialize spotify

--- liri.js
+++ liri.js
@@ -1,1 +1,18 @@
-var keys = require("./keys.js");
+//requirements
+
+let axios = require("axios");
+let Spotify = require("node-spotify-api");
+let dotenv = require('dotenv').config();
+let moment = require('moment');
+let fs = require('fs');
+
+//import from keys
+let keys = require ('keys.js');
+let spotify2 = new Spotify(keys.Spotify);
+
+//command line arguments
+
+moment().format();
+
+//npm installs and initialize package.json
+
